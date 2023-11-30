b8e5d0deddc04fa0a0d46a103a1c40081c751afe
Besim Gurbuz <contact@besimgurbuz.dev>
2023-11-30 23:23:39 +0100

    update tools

--- src/routes/api/tools/+server.ts
+++ src/routes/api/tools/+server.ts
@@ -5,70 +5,69 @@ const TOOLS: Tool[] = [
 	{
 		label: 'TypeScript',
 		types: ['frontend', 'backend'],
-		iconUrl:
-			'./typescript-icon.png'
+		iconUrl: './typescript-icon.png'
 	},
 	{
 		label: 'JavaScript',
 		types: ['frontend', 'backend'],
-		iconUrl:
-			'./javascript-icon.png'
+		iconUrl: './javascript-icon.png'
 	},
 	{
 		label: 'Angular',
 		types: ['frontend'],
-		iconUrl:
-			'./angular-icon.png'
+		iconUrl: './angular-icon.png'
 	},
 	{
 		label: 'Svelte',
 		types: ['frontend'],
-		iconUrl:
-			'./svelte-icon.png'
+		iconUrl: './svelte-icon.png'
 	},
 	{
 		label: 'Node.js',
 		types: ['backend'],
-		iconUrl:
-			'./nodejs-icon.png'
+		iconUrl: './nodejs-icon.png'
 	},
 	{
 		label: 'Sass',
 		types: ['frontend'],
-		iconUrl:
-			'./sass-icon.png'
+		iconUrl: './sass-icon.png'
 	},
 	{
 		label: 'Java',
 		types: ['backend'],
-		iconUrl:
-			'./java-icon.png'
+		iconUrl: './java-icon.png'
 	},
 	{
 		label: 'Spring Boot',
 		types: ['backend'],
-		iconUrl:
-			'./spring-boot-icon.png'
+		iconUrl: './spring-boot-icon.png'
 	},
 	{
 		label: 'PostgreSQL',
 		types: ['backend', 'database'],
-		iconUrl:
-			'./postgres-icon.png'
+		iconUrl: './postgres-icon.png'
 	},
 	{
 		label: 'Go',
 		types: ['backend', 'system_programming'],
-		iconUrl:
-			'./go-icon.png',
+		iconUrl: './go-icon.png',
 		inProgress: true
 	},
 	{
 		label: 'Rust',
 		types: ['backend', 'system_programming'],
-		iconUrl:
-			'./rust-icon.png',
+		iconUrl: './rust-icon.png',
 		inProgress: true
+	},
+	{
+		label: 'Docker',
+		types: ['backend'],
+		iconUrl: './docker-icon.png'
+	},
+	{
+		label: 'Kubernetes',
+		types: ['backend'],
+		iconUrl: './k8s-icon.png'
 	},
 	{
 		label: 'Tailwind',
